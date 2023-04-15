571de127786abd947abf6e3957052c9bd257b7c9
Peter Eisenmann <p3732@getgoogleoff.me>
2023-04-16 00:07:49 +0200

    directory-asycn: drop 13 year long unused code

--- src/nautilus-directory-async.c
+++ src/nautilus-directory-async.c
@@ -279,62 +279,6 @@ request_counter_remove_request (RequestCounter counter,
     }
 }
 
-#if 0
-static void
-nautilus_directory_verify_request_counts (NautilusDirectory *directory)
-{
-    GList *l;
-    RequestCounter counters;
-    int i;
-    gboolean fail;
-    GHashTableIter monitor_iter;
-    gpointer value;
-
-    fail = FALSE;
-    for (i = 0; i < REQUEST_TYPE_LAST; i++)
-    {
-        counters[i] = 0;
-    }
-    g_hash_table_iter_init (&monitor_iter, directory->details->monitor_table);
-    while (g_hash_table_iter_next (&monitor_iter, NULL, &value))
-    {
-        for (l = value; l; l = l->next)
-        {
-            Monitor *monitor = l->data;
-            request_counter_add_request (counters, monitor->request);
-        }
-    }
-    for (i = 0; i < REQUEST_TYPE_LAST; i++)
-    {
-        if (counters[i] != directory->details->monitor_counters[i])
-        {
-            g_warning ("monitor counter for %i is wrong, expecting %d but found %d",
-                       i, counters[i], directory->details->monitor_counters[i]);
-            fail = TRUE;
-        }
-    }
-    for (i = 0; i < REQUEST_TYPE_LAST; i++)
-    {
-        counters[i] = 0;
-    }
-    for (l = directory->details->call_when_ready_list; l != NULL; l = l->next)
-    {
-        ReadyCallback *callback = l->data;
-        request_counter_add_request (counters, callback->request);
-    }
-    for (i = 0; i < REQUEST_TYPE_LAST; i++)
-    {
-        if (counters[i] != directory->details->call_when_ready_counters[i])
-        {
-            g_warning ("call when ready counter for %i is wrong, expecting %d but found %d",
-                       i, counters[i], directory->details->call_when_ready_counters[i]);
-            fail = TRUE;
-        }
-    }
-    g_assert (!fail);
-}
-#endif
-
 /* Start a job. This is really just a way of limiting the number of
  * async. requests that we issue at any given time. Without this, the
  * number of requests is unbounded.
