b92efed3dc4087abad61082b461c45ee2f356bf2
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2024-10-09 10:02:51 +0300

    gtk/placessidebar: Don't open insensitive rows
    Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2706

--- src/gtk/nautilusgtkplacessidebar.c
+++ src/gtk/nautilusgtkplacessidebar.c
@@ -1117,7 +1117,9 @@ hover_timer (gpointer user_data)
                                                  NAUTILUS_PREFERENCES_OPEN_FOLDER_ON_DND_HOVER);
   sidebar->hover_timer_id = 0;
 
-  if (open_folder_on_hover && sidebar->hover_row != NULL)
+  if (open_folder_on_hover &&
+      sidebar->hover_row != NULL &&
+      gtk_widget_get_sensitive (GTK_WIDGET (sidebar->hover_row)))
     {
       g_object_get (sidebar->hover_row, "uri", &uri, NULL);
       if (uri != NULL && g_strcmp0 (uri, SCHEME_TRASH ":///") != 0)
@@ -1300,7 +1302,8 @@ drag_motion_callback (GtkDropTarget    *target,
   row = gtk_list_box_get_row_at_y (GTK_LIST_BOX (sidebar->list_box), y);
 
   start = sidebar->hover_start_point;
-  if (sidebar->drag_row == NULL &&
+  if (row != NULL &&
+      sidebar->drag_row == NULL &&
       (row != sidebar->hover_row ||
       gtk_drag_check_threshold (GTK_WIDGET (sidebar), start.x, start.y, x, y)))
     {
